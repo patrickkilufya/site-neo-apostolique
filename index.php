99ac063db82d3d422cb325b52f88efc98df72656
patrickkilufya <patrickkilufya1@gmail.com>
2019-06-27 22:18:39 +0200

    creation du design de presentation web

---
+++ index.php
@@ -0,0 +1,388 @@
+<!doctype html>
+<html>
+    <head>
+        <title>ENAC-SUD/EST</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="Accept-Charset">
+        <link href="css/bootstrap-theme.css" rel="stylesheet"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <link href="css/navbar-static-top.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <!-- //for-mobile-apps -->
+    </head>
+
+    <body>
+    <div class="glogo">
+        <div class="container">
+            <div class="logo">
+                <a href="index.php"><span>ENA</span>RDC/SUD-EST</a>
+            </div>
+            <div class="logo1">
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2 d" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0 b" type="submit">Search</button>
+                </form>
+
+            </div>
+        </div>
+
+
+    <div class="header">
+
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">neo</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Accueil</a></li>
+                        <li><a href="#about">Actualite</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Eglise<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Vision</a></li>
+                                <li><a href="">Mission</a></li>
+                                <li><a href="">Doctrine</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Postorale<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Ecole du dimanche</a></li>
+                                <li><a href="">Jeunesse</a></li>
+                                <li><a href="">Chorale</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#about">Construction</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Organisation<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Spirituel</a></li>
+                                <li><a href="">Administrative</a></li>
+                                <li><a href="">Chorale</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#about">blog</a></li>
+
+                    </ul>
+
+
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+    </div>
+    </div>
+    <div class="home">
+
+    </div>
+    <!---------------Fin entete----------------------------->
+    <!---------------debut corps-------------------------->
+
+
+
+
+<div class="col-lg-12">
+
+    <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12" >
+        <section class="slider" id="home">
+            <div class="container-fluid">
+                <div class="row">
+
+                    <div id="carouselHacked" class="carousel slide carousel-fade" data-ride="carousel">
+                        <div class="header-backup"></div>
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="images/co1.JPG"  width="100%" alt="">
+                                <div class="carousel-caption">
+                                    <h1>Ordination</h1>
+                                    <p>Apd TSHITSHI</p>
+                                    <button>plus d'info</button>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="images/co1.JPG" alt="">
+                                <div class="carousel-caption">
+                                    <h1>Ordination</h1>
+                                    <p>Apd TSHITSHI</p>
+                                    <button>plus d'info</button>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="images/co1.JPG" alt="">
+                                <div class="carousel-caption">
+                                    <h1>Ordination</h1>
+                                    <p>Apd TSHITSHI</p>
+                                    <button>plus d'info</button>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="images/co1.JPG" alt="">
+                                <div class="carousel-caption">
+                                    <h1>Ordination</h1>
+                                    <p>Apd TSHITSHI</p>
+                                    <button>plus d'info</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carouselHacked" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carouselHacked" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </section><!-- end of slider section -->
+    </div>
+        <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <div class="block-3">
+            <div class="block-3-left">
+                <div class="historique-img">
+
+                </div>
+
+                <div class="bienvenue-text banner-bottom-grid-box3">
+                    <h3 class="banner-bottom-grid-box1">EVENEMENT </h3>
+                    <p align="justify">À son retour de la République démocratique du Congo,
+                        l’apôtre-patriarche adjoint Jean-Luc Schneider se dit reconnaissant et soulagé :
+                        le service divin solennel qu’il a célébré à Lubumbashi a été un temps fort grandiose de ce début de l’année 2013.</p>
+                        <p align="justify">Il a investi son successeur Tshitshi Tshisekedi du ministère d’apôtre de district (nous en avions rendu compte).</p>
+
+
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+        <div class="well"></div>
+    </div>
+    <!----------------------------------------------------------------------------------------------------------------------->
+
+    <!-- about section -->
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+    <section class="about text-center" id="about">
+        <div class="neo">
+            <div class="row">
+                <div class="container neo">
+                <h2 align="center">L' église néo-apostolique</h2>
+                <h4>L’Eglise néo-apostolique, dont le siège mondial est implanté à Zurich (Suisse),
+                    est présente dans la plupart des pays du globe.
+                    Elle compte environ onze millions de membres. Elle est dirigée par des apôtres,
+                    sous l’autorité de l’apôtre-patriarche. </h4>
+                <h4>L’Eglise administre trois sacrements : le Saint-Baptême d’eau,
+                    le Saint-Scellé (dispensation du Saint-Esprit par l’imposition des mains et la prière d’un apôtre), la Sainte cène.
+                    Elle prépare ses fidèles en vue du retour proche de Jésus-Christ.</h4>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-about-detail clearfix">
+                        <div class="about-img">
+                            <img src="images/co1.jpg" alt="">
+                        </div>
+
+                        <div class="about-details">
+                            <div class="pentagon-text">
+                                <h1>D</h1>
+                            </div>
+
+                            <h3>Doctrine</h3>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-about-detail">
+                        <div class="about-img">
+                            <img class="img-responsive" src="images/co1.jpg" alt="">
+                        </div>
+
+                        <div class="about-details">
+                            <div class="pentagon-text">
+                                <h1>V</h1>
+                            </div>
+
+                            <h3>Vision</h3>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-about-detail">
+                        <div class="about-img">
+                            <img class="img-responsive" src="images/co1.jpg" alt="">
+                        </div>
+
+                        <div class="about-details">
+                            <div class="pentagon-text">
+                                <h1>M</h1>
+                            </div>
+
+                            <h3>Mission</h3>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section><!-- end of about section -->
+    </div>
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+
+        <section class="letast_news neo">
+            <div class="letast_entete "><h2>INFORMATIONS RECENTES</h2>
+            <p>Les activités de l'apotre de District <br> les activités qui  </p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="single_news">
+                            <img src="images/co1.JPG" alt="">
+                            <div class="texts">
+                                <p class="date"><a href="#">20 Mai 2019</a></p>
+                                <h3>CONFIRMATION ET RETRAITE A KOLWEZI  <br> AVEC L’APOTRE DE DISTRICT TSHISEKEDI</h3>
+                                <p class="test" align="justfy">Le dimanche 05 mai 2019 restera une date inoubliable dans l’histoire de l’Eglise à Kolwezi
+                                    (Champ d’activités de l’apôtre Musuwa Joseph), où a eu lieu un double évènement, savoir la dispensation
+                                    de la bénédiction de confirmation à 165 confirmands, et la retraite de l’évêque Ilunga Amisi Barthélemy...</p>
+                                <h3><a href="#">LIRE LA SUITE</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single_news">
+                            <img src="images/co1.JPG" alt="">
+                            <div class="texts">
+                                <p class="date"><a href="#">06 Mars 2019</a></p>
+                                <h3>L’Apôtre Patriarche Jean-Luc Schneider  <br> célèbre le service divin en faveur des défunts au Burundi</h3>
+                                <p class="test">Pour la première fois, depuis la création des premières communautés néo-apostoliques en 1990,
+                                    année où l'œuvre est entrée dans le pays, la République du Burundi, a connu la première visite d'un apôtre patriarche.
+                                    Du 01 au 03 mars 2019, le primat de l'Église, l'Apôtre Patriarche Jean-Luc Schneider, a visité ...</p>
+                                <h3><a href="#">LIRE LA SUITE</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single_news">
+                            <img src="images/co1.JPG" alt="">
+                            <div class="texts">
+                                <p class="date"><a href="#">11 Décembre 2018</a></p>
+                                <h3>La Communauté Plateau reçoit  <br> l’Apôtre de District</h3>
+                                <p class="test">A l’issue de la 17ème réunion des apôtres responsables de l’ENA RDC SUD-EST,
+                                    qui était aussi la dernière pour cette année 2018,
+                                    l’apôtre de district Tshisekedi, afin terminer l’année en beauté, s’est arrêté à Plateau, ...</p>
+                                <h3><a href="#">LIRE LA SUITE</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+<div class="col-lg-12">
+    <!-- map section -->
+    <section class="api-map" id="contact">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 map" id="map"></div>
+            </div>
+        </div>
+    </section><!-- end of map section -->
+
+    <!-- contact section starts here -->
+    <section class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="contact-caption clearfix">
+                    <div class="contact-heading text-center">
+                        <h2>contact us</h2>
+                    </div>
+
+                    <div class="col-md-5 contact-info text-left">
+                        <h3>contact information</h3>
+                        <div class="info-detail">
+                            <ul><li><i class="fa fa-calendar"></i><span>Monday - Friday:</span> 9:30 AM to 6:30 PM</li></ul>
+                            <ul><li><i class="fa fa-map-marker"></i><span>Address:</span> 123 Some Street , California, US, CP 123</li></ul>
+                            <ul><li><i class="fa fa-phone"></i><span>Phone:</span> (01) 999-1235</li></ul>
+                            <ul><li><i class="fa fa-fax"></i><span>Fax:</span> (01) 999-1234</li></ul>
+                            <ul><li><i class="fa fa-envelope"></i><span>Email:</span> info@domain.com</li></ul>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 col-md-offset-1 contact-form">
+                        <h3>leave us a message</h3>
+
+                        <form class="form">
+                            <input class="name" type="text" placeholder="Name">
+                            <input class="email" type="email" placeholder="Email">
+                            <input class="phone" type="text" placeholder="Phone No:">
+                            <textarea class="message" name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                            <input class="submit-btn" type="submit" value="SUBMIT">
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section><!-- end of contact section -->
+
+</div>
+   <div class="col-lg-12"> <!-- footer starts here -->
+    <footer class="footer clearfix">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 footer-para">
+                    <p>&copy; <a href="https://www.ena-rdcsudest.org">Eglise Neo Apostolique</a> All right reserved</p>
+                </div>
+
+                <div class="col-xs-6 text-right">
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                    <a href=""><i class="fa fa-skype"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+   </div>
+
+
+
+    <!-- Bootstrap core JavaScript
+   ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.2.1.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    </body>
+</html>
---
+++ question.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html>
+<head>
+    <title>ENAC-SUD/EST</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="Accept-Charset">
+    <link href="css/bootstrap-theme.css" rel="stylesheet"/>
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="css/navbar-static-top.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //for-mobile-apps -->
+</head>
+
+<body>
+<div class="glogo">
+    <div class="container">
+        <div class="logo">
+            <a href="index.php"><span>ENA</span>RDC/SUD-EST</a>
+        </div>
+        <div class="logo1">
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2 d" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0 b" type="submit">Search</button>
+            </form>
+
+        </div>
+    </div>
+
+
+    <div class="header">
+
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">neo</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Accueil</a></li>
+                        <li><a href="#about">Actualite</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Eglise<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Vision</a></li>
+                                <li><a href="">Mission</a></li>
+                                <li><a href="">Doctrine</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Postorale<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Ecole du dimanche</a></li>
+                                <li><a href="">Jeunesse</a></li>
+                                <li><a href="">Chorale</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#about">Construction</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Organisation<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Spirituel</a></li>
+                                <li><a href="">Administrative</a></li>
+                                <li><a href="">Chorale</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#about">blog</a></li>
+
+                    </ul>
+
+
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+    </div>
+</div>
+<div class="home">
+
+</div>
+<!---------------Fin entete----------------------------->
+
+<form>
+    <input>
+    <input>
+    <input>
+    <input>
+</form>
